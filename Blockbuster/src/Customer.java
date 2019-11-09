2f2cd15edec64dd88fbd9f30dc548508858029c8
Breno Poggiali <brenopoggiali@gmail.com>
2019-11-09 12:07:37 -0300

    Add 5th commit

--- Blockbuster/src/Customer.java
+++ Blockbuster/src/Customer.java
@@ -26,7 +26,7 @@ public class Customer {
 	        double thisAmount = 0;
 	        Rental each = (Rental) rentals.nextElement();
 
-	        thisAmount = amountFor(each);
+	        thisAmount = each.getCharge();
 
 	        // add frequent renter points
 	        frequentRenterPoints ++;
@@ -46,25 +46,4 @@ public class Customer {
 	             " frequent renter points";
 	     return result;
 	  }
-	   
-	  public double amountFor(Rental each){
-		   double thisAmount = 0;
-		   //determine amounts for each line
-		   switch (each.getMovie().getPriceCode()) {
-		   		case Movie.REGULAR:
-		   			thisAmount += 2;
-		   			if (each.getDaysRented() > 2)
-		   				thisAmount += (each.getDaysRented() - 2) * 1.5;
-		   			break;
-		   		case Movie.NEW_RELEASE:
-		   			thisAmount += each.getDaysRented() * 3;
-		   			break;
-		   		case Movie.CHILDRENS:
-		   			thisAmount += 1.5;
-		   			if (each.getDaysRented() > 3)
-		   				thisAmount += (each.getDaysRented() - 3) * 1.5;
-		   			break;
-		   }
-		   return thisAmount;
-	   }
 }
